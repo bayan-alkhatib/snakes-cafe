bb6a1c6171a4d7c1a15da3ba82a3dd3a5e8d238e
bayan-alkhatib <bayankhatibtr@gmail.com>
2021-06-06 21:47:41 +0300

    complete lab 01 and strech goal

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -32,23 +32,30 @@ Tea
 Unicorn Tears """)
 
 i=0
+count_order=1
 orderArr=[]
 menu=['wings','cookies','spring rolls','salmon','steak','meat tornado','a literal garden','ice cream','cake','pie','coffee','tea','unicorn tears']
-while i>=0 :
-    order= input("""
+
+print("""
     ***********************************
     ** What would you like to order? **
     ***********************************
-    > """)
+    """)
+
+while i>=0:
+    order=input('> ')
     if order.lower() in menu:
-        i+=1
-        print('** %d order of %s have been added to your meal **' %(i,order))
+        for y in range(0,len(orderArr)):
+            if order == orderArr[y]:
+                count_order+=1
+        print('** %d order of %s have been added to your meal **' %(count_order,order))
         orderArr.append(order)
+        count_order=1
     elif order == 'quit':
         break      
     else:
         print('** Your Order is not in the menu! Please choose from the above menu **')
-        
+    
 print('your orders are ')
 for j in range(0,len(orderArr)) :
     print(orderArr[j])
